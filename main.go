1e89d8dcc6b9aadf7a3eccf950641397e0ffc019
Ploum <keo.ployer@ynov.com>
2024-12-16 11:35:33 +0100

    chemin css qui marche normalement

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	e "engine/serveur"
+	"net/http"
 )
 
 func main() {
+	css := http.FileServer(http.Dir("css"))
+	http.Handle("/css/", http.StripPrefix("/css/", css))
 	var jeu e.Engine
 	e.Run(&jeu)
 }
