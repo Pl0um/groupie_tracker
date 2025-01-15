2868e796a841a43f178939ca72c70c4a194da3bf
unknown <mael.guillaume.34@gmail.com>
2025-01-15 19:06:34 +0100

    Simplify api

--- serveur/groupie.go
+++ serveur/groupie.go
@@ -3,23 +3,16 @@ package engine
 import (
 	"html/template"
 	"net/http"
-	"path/filepath"
 )
 
 func (base *Engine) Groupie(w http.ResponseWriter, r *http.Request) {
 	// Définir le chemin complet vers le fichier Groupie.html
-	tmplPath := filepath.Join("template", "Groupie.html")
+	tmpl := template.Must(template.ParseFiles("template/Groupie.html"))
 
-	// Utiliser la bibliothèque template pour créer un modèle qui recherche mon fichier Groupie.html
-	tmpl, err := template.ParseFiles(tmplPath)
-	if err != nil {
-		http.Error(w, "Erreur lors du chargement du modèle", http.StatusInternalServerError)
-		return
-	}
+	// Je crée une structure de données pour les données du template
+	data := Engine{}
 
-	// Exécuter le modèle avec des données
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		http.Error(w, "Erreur lors de l'exécution du modèle", http.StatusInternalServerError)
-	}
+	// J'execute le template avec les données
+	tmpl.Execute(w, data)
 }
+
--- serveur/run.go
+++ serveur/run.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func Run(base *Engine) {
@@ -10,22 +11,27 @@ func Run(base *Engine) {
     http.HandleFunc("/", base.Handler) // Une fois lancer on arrive sur la racine ou on appelle la fonction Handler
     http.HandleFunc("/Home", base.Home)
     http.HandleFunc("/Groupie", base.Groupie)
-    http.HandleFunc("/Credit", base.Credit)
-    http.HandleFunc("/Locations", base.Location)
+    
 
-    http.HandleFunc("/api/artists", func(w http.ResponseWriter, r *http.Request) {
-        res := GetApi("https://groupietrackers.herokuapp.com/api/artists")
+    http.HandleFunc("/api/artists/", func(w http.ResponseWriter, r *http.Request) {
+        id := strings.TrimPrefix(r.URL.Path, "/api/artists/")
+        res := GetApi("https://groupietrackers.herokuapp.com/api/artists" + id)
         w.Header().Set("Content-Type", "application/json")
         w.Write(res)
     })
-    http.HandleFunc("/api/locations", func(w http.ResponseWriter, r *http.Request) {
-        res := GetApi("https://groupietrackers.herokuapp.com/api/locations")
-        w.Header().Set("Content-Type", "application/json")
-        w.Write(res)
-    })
+    
 	css := http.FileServer(http.Dir("css"))
 	http.Handle("/css/", http.StripPrefix("/css/", css))
 
+    // Servir les fichiers images
+    images := http.FileServer(http.Dir("images"))
+    http.Handle("/images/", http.StripPrefix("/images/", images))
+
+    // Servir le fichier Groupie.html
+    http.HandleFunc("/Groupie.html", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "template/Groupie.html")
+    })
+    
     fmt.Println("Le serveur c'est lancer ici : http://localhost:2025") // J'affiche un message pour dire que le serveur c'est lancer
     if err := http.ListenAndServe(":2025", nil); err != nil { // Je lance le serveur sur le port 2025
         fmt.Println("Désoler le serveur ne c'est pas lancer :", err) // Si il y a une erreur je l'affiche
