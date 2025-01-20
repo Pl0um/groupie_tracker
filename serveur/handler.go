37c0c70f37615d68eb7419b93516ebd815d8c010
Howexou <l.cartagena1221@gmail.com>
2025-01-20 14:34:38 +0100

    création crédits

--- serveur/handler.go
+++ serveur/handler.go
@@ -1,15 +1,14 @@
 package engine
 
 import (
-	"html/template"
-	"net/http"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
-	
 )
 
 func (base *Engine) Home(w http.ResponseWriter, r *http.Request) {
@@ -20,18 +19,17 @@ func (base *Engine) Home(w http.ResponseWriter, r *http.Request) {
 	}
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
 
-
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 	var GroupList list
 	json.Unmarshal(responseData, &GroupList.Lists)
-	t.Execute(w,GroupList)
+	t.Execute(w, GroupList)
 }
 
 func Groupie(w http.ResponseWriter, r *http.Request) {
@@ -43,18 +41,22 @@ func Groupie(w http.ResponseWriter, r *http.Request) {
 
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
 
-
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 	var GroupList []Engine
 	json.Unmarshal(responseData, &GroupList)
 
 	id, _ := strconv.Atoi(r.FormValue("id"))
-	t.Execute(w,GroupList[id-1])
+	t.Execute(w, GroupList[id-1])
+}
+
+func Credit(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("template/Credit.html"))
+	tmpl.Execute(w, nil)
 }
